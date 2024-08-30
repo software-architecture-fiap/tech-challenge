b45ab075b50641061580363c0877c1c2875bc7a7
rikemorais <rikeaju@hotmail.com>
2024-08-29 21:05:01 -0300

    style: Docstring e Type Hints

--- alembic/env.py
+++ alembic/env.py
@@ -3,9 +3,11 @@ from __future__ import with_statement
 import os
 import sys
 from logging.config import fileConfig
+from typing import Optional
 
 from dotenv import load_dotenv
 from sqlalchemy import engine_from_config, pool
+from sqlalchemy.engine import Engine
 
 from alembic import context
 from app.db.database import Base
@@ -13,23 +15,26 @@ from app.db.database import Base
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 load_dotenv()
-
 config = context.config
-
-# Configurações de logging
 fileConfig(config.config_file_name)
-
-# Metadata para autogeração
 target_metadata = Base.metadata
 
 
-# Obter URL do banco de dados a partir da variável de ambiente
-def get_url():
+def get_url() -> Optional[str]:
+    """Obter URL do banco de dados a partir da variável de ambiente.
+
+    Returns:
+        Optional[str]: A URL do banco de dados, se definida.
+    """
     return os.getenv('DATABASE_URL')
 
 
-def run_migrations_offline():
-    """Executa as migrações no modo offline."""
+def run_migrations_offline() -> None:
+    """Executa as migrações no modo offline.
+
+    A URL do banco de dados é obtida e usada para configurar o contexto de migração.
+    As migrações são então executadas sem necessidade de conexão ativa com o banco de dados.
+    """
     url = get_url()
     context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
 
@@ -37,9 +42,13 @@ def run_migrations_offline():
         context.run_migrations()
 
 
-def run_migrations_online():
-    """Executa as migrações no modo online."""
-    connectable = engine_from_config(
+def run_migrations_online() -> None:
+    """Executa as migrações no modo online.
+
+    Um engine é criado e usado para conectar ao banco de dados. As migrações
+    são executadas dentro de uma transação ativa.
+    """
+    connectable: Engine = engine_from_config(
         config.get_section(config.config_ini_section), prefix='sqlalchemy.', poolclass=pool.NullPool, url=get_url()
     )
 
