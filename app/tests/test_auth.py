3d0de9eb956289d8335bbeb8132e102be935eb80
rikemorais <rikeaju@hotmail.com>
2024-09-08 17:06:07 -0300

    test: Adiciona Testes para a Rota Auth

---
+++ app/tests/test_auth.py
@@ -0,0 +1,36 @@
+from types import SimpleNamespace
+
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_auth_success(mocker):
+    mock_user = SimpleNamespace(id=1, email='user@example.com', hashed_password='$2b$12$125as3fd45gdas5')
+
+    mocker.patch('app.services.repository.get_user_by_email', return_value=mock_user)
+    mocker.patch('app.services.security.verify_password', return_value=True)
+    mocker.patch('app.services.security.create_access_token', return_value='mock_access_token')
+
+    form_data = {'username': 'user@example.com', 'password': 'valid_password'}
+
+    response = client.post('/token', data=form_data)
+
+    assert response.status_code == status.HTTP_200_OK
+    print('Response JSON:', response.json())
+    assert response.json() == {'access_token': 'bearer mock_access_token', 'customer_id': mock_user.id}
+
+
+def test_auth_invalid_credentials(mocker):
+    mock_user = None
+    mocker.patch('app.services.repository.get_user_by_email', return_value=mock_user)
+
+    form_data = {'username': 'invalid@example.com', 'password': 'wrong_password'}
+
+    response = client.post('/token', data=form_data)
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert response.json() == {'detail': 'Nome de usuário ou senha incorretos'}
